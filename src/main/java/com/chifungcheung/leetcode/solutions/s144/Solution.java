410a382e43d18f6eb6883b388766735d5f8107d7
Chifung Cheung <chifung.cheung@gmail.com>
2019-10-20 00:26:26 +0900

    s144

---
+++ src/main/java/com/chifungcheung/leetcode/solutions/s144/Solution.java
@@ -0,0 +1,36 @@
+package com.chifungcheung.leetcode.solutions.s144;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode id=144 lang=java
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode cur = stack.pop();
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            ans.add(cur.val);
+        }
+        return ans;
+    }
+}
+// @lc code=end
---
+++ src/test/java/com/chifungcheung/leetcode/solutions/s144/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.chifungcheung.leetcode.solutions.s144;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void testSolution() {
+    Solution sol = new Solution();
+    TreeNode root = new TreeNode(1);
+    root.right = new TreeNode(2);
+    root.right.left = new TreeNode(3);
+    List<Integer> ans = sol.preorderTraversal(root);
+
+    assertArrayEquals(new Integer[] { 1, 2, 3 }, ans.toArray());
+  }
+}
