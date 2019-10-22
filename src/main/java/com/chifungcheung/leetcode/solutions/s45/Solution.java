7f18e484c53f1153af0165256a1dc0ac4d4ef367
Chifung Cheung <chifung.cheung@gmail.com>
2019-10-22 23:16:42 +0900

    s45

--- src/main/java/com/chifungcheung/leetcode/solutions/s45/Solution.java
+++ src/main/java/com/chifungcheung/leetcode/solutions/s45/Solution.java
@@ -8,9 +8,29 @@ package com.chifungcheung.leetcode.solutions.s45;
 
 // @lc code=start
 class Solution {
-    public int jump(int[] nums) {
+  public int jump(int[] nums) {
+    int n = nums.length;
+    if (n <= 1) {
       return 0;
     }
+
+    int[] d = new int[n];
+    d[n - 1] = 0;
+
+    for (int i = n - 2; i >= 0; i--) {
+      int minStep = Integer.MAX_VALUE;
+      int jump = 1;
+      while (jump <= nums[i] && i + jump < n) {
+        minStep = Math.min(d[i + jump], minStep);
+        jump++;
+      }
+      if (minStep != Integer.MAX_VALUE) {
+        d[i] = minStep + 1;
+      } else {
+        d[i] = Integer.MAX_VALUE;
+      }
+    }
+    return d[0];
+  }
 }
 // @lc code=end
-
--- src/test/java/com/chifungcheung/leetcode/solutions/s45/SolutionTest.java
+++ src/test/java/com/chifungcheung/leetcode/solutions/s45/SolutionTest.java
@@ -8,8 +8,29 @@ public class SolutionTest {
   @Test
   public void testSolution001() {
     Solution sol = new Solution();
-    //  The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
-    int ans = sol.jump(new int[]{2,3,1,1,4});
+    // The minimum number of jumps to reach the last index is 2. Jump 1 step from
+    // index 0 to 1, then 3 steps to the last index.
+    int ans = sol.jump(new int[] { 2, 3, 1, 1, 4 });
     assertEquals(2, ans);
+  }
+
+  @Test
+  public void testSolution002() {
+    Solution sol = new Solution();
+    int ans = sol.jump(new int[] { 2, 3, 0, 1, 4 });
+    assertEquals(2, ans);
+  }
+
+  @Test
+  public void testSolution003() {
+    Solution sol = new Solution();
+    assertEquals(0, sol.jump(new int[] { 0 }));
+    assertEquals(0, sol.jump(new int[] { 1 }));
+  }
+
+  @Test
+  public void testSolution004() {
+    Solution sol = new Solution();
+    assertEquals(3, sol.jump(new int[] { 5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0 }));
   }
 }
