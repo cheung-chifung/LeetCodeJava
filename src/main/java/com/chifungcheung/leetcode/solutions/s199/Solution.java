cc2523da237d3191724d7f0e365be72fe26b3a09
Chifung Cheung <chifung.cheung@gmail.com>
2019-10-26 15:07:37 +0900

    s199

---
+++ src/main/java/com/chifungcheung/leetcode/solutions/s199/Solution.java
@@ -0,0 +1,22 @@
+package com.chifungcheung.leetcode.solutions.s199;
+
+/*
+ * @lc app=leetcode id=198 lang=java
+ *
+ * [198] House Robber
+ */
+
+// @lc code=start
+class Solution {
+  public int rob(int[] nums) {
+    int n = nums.length;
+    int[] d = new int[n + 2];
+    d[0] = 0;
+    d[1] = 0;
+    for (int i = 2; i < n + 2; i++) {
+      d[i] = Math.max(d[i - 2] + nums[i - 2], d[i - 1]);
+    }
+    return d[n + 1];
+  }
+}
+// @lc code=end
---
+++ src/test/java/com/chifungcheung/leetcode/solutions/s199/SolutionTest.java
@@ -0,0 +1,23 @@
+package com.chifungcheung.leetcode.solutions.s199;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+  @Test
+  public void testSolution001() {
+    Solution sol = new Solution();
+
+    int ans = sol.rob(new int[] { 1, 2, 3, 1 });
+    assertEquals(4, ans);
+  }
+
+  @Test
+  public void testSolution002() {
+    Solution sol = new Solution();
+
+    int ans = sol.rob(new int[] { 2, 7, 9, 3, 1 });
+    assertEquals(12, ans);
+  }
+}
