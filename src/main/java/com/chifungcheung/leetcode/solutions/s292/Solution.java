e9e96e832f371199c6372bdcd62786256e467303
Chifung Cheung <chifung.cheung@gmail.com>
2019-11-11 22:23:27 +0900

    s292

---
+++ src/main/java/com/chifungcheung/leetcode/solutions/s292/Solution.java
@@ -0,0 +1,15 @@
+package com.chifungcheung.leetcode.solutions.s292;
+
+/*
+ * @lc app=leetcode id=292 lang=java
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+class Solution {
+  public boolean canWinNim(int n) {
+    return n % 4 != 0;
+  }
+}
+// @lc code=end
